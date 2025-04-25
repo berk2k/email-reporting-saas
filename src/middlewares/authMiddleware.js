3fed56a94a527b37fa6e45be510461465294e878
Berk Polat <berkpolat125@gmail.com>
2025-04-25 19:58:02 +0300

    update

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -9,17 +9,17 @@ const authMiddleware = (req, res, next) => {
     return res.status(401).json({ message: 'Access denied. No token provided.' });
   }
 
-  // Token doğrulama işlemi
+
   try {
     const decoded = jwt.verify(token, config.jwtSecret);
     
-    // Token doğruysa kullanıcıyı request'e ekle
+
     req.user = decoded;
     console.log('Token successfully verified:', decoded);
     
     next();
   } catch (error) {
-    // Hata türüne göre mesaj ver
+
     if (error.name === 'JsonWebTokenError') {
       console.error('JWT Error: Invalid token format or signature.');
       return res.status(400).json({ message: 'Invalid token format or signature.' });
@@ -30,7 +30,7 @@ const authMiddleware = (req, res, next) => {
       return res.status(401).json({ message: 'Token has expired. Please login again.' });
     }
 
-    // Diğer hatalar için genel mesaj
+
     console.error('JWT Error:', error.message);
     return res.status(400).json({ message: 'Invalid token.' });
   }
