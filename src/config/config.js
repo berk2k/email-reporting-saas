9a4184a51787f8eafbf3ce3b79dcb41f0ee9230e
Berk Polat <berkpolat125@gmail.com>
2025-04-28 16:23:04 +0300

    email service added

--- src/config/config.js
+++ src/config/config.js
@@ -7,4 +7,6 @@ export const config = {
   jwtSecret: process.env.JWT_SECRET_KEY,
   dbUrl: process.env.DATABASE_URL,
   nodeEnv: process.env.NODE_ENV || 'development',
+  emailUser:process.env.EMAIL_USER,
+  emailPass:process.env.EMAIL_PASSWORD
 };
---
+++ src/services/emailService.js
@@ -0,0 +1,29 @@
+import nodemailer from 'nodemailer';
+import { config } from '../config/config.js';
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth:{
+        user: config.emailUser,
+        pass: config.emailPass
+    },
+});
+
+export const sendEmail = async (to, subject, text, html) => {
+    try {
+      const mailOptions = {
+        from: config.emailUser,
+        to,
+        subject,
+        text,
+        html,
+      };
+  
+      // Send the email
+      await transporter.sendMail(mailOptions);
+      console.log('Email sent successfully');
+    } catch (error) {
+      console.error('Error sending email:', error);
+      throw new Error('Email sending failed');
+    }
+  };
